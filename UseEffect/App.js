ec26208c88c80a33c92b45472d3f9d1da8a96406
Tamim khan <mdtamimkhan842@gmail.com>
2025-03-19 12:59:08 +0600

    added useEffect

---
+++ UseEffect/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className='app'>
+                <div>We shall learn useEffect() today</div>
+            </div >
+        );
+    }
+}
---
+++ UseEffect/Component/MycomponentClass.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class mycomponent extends React.Component {
+
+    state = {
+        count: 0,
+        date: new Date(),
+    }
+
+    // now using function
+    //this funciton work one time
+    componentDidMount() {
+        const { count } = this.state;
+        document.title = `Clicked ${count} times`;
+    }
+    //then this function work for updating the value
+    componentDidUpdate() {
+        const { count } = this.state;
+        document.title = `Clicked ${count} times`;
+    }
+
+    addclick = () => {
+        // setstate find previous state value and then we will be update the value 
+        // previous value theke count ar value ta destructure kore rekhe dilam
+        this.setState(({ count }) => ({
+            count: count + 1,
+        }));
+    };
+
+
+    // now use tick fuction which updating the current value of the date 
+
+    tick = () => {
+        this.setState({
+            date: new Date(),
+        });
+    };
+
+    render() {
+
+        const { date } = this.state;
+
+        return (
+            <div>
+                {/*  toLocaleDateString Get a date as a string, using locale conventions: */}
+                <p>  Time: {date.toLocaleDateString()}</p>
+                <p>
+                    <button type='button' onClick={this.addclick}>Click</button>
+                </p>
+            </div>
+        );
+    }
+}
+
+export default mycomponent;
